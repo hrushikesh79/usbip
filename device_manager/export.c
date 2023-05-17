8e8d60e698e74a6ce8730367e2e62e221e6318ed
hrushikesh79 <133630525+hrushikesh79@users.noreply.github.com>
2023-05-17 19:18:00 +0530

    Update export.c

--- device_manager/export.c
+++ device_manager/export.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only
  *
- * usbip project file
+ * source file contains bind and unbind function to bind and unbind
+ * the usb device 
  *
  * Copyright (C) 2023 Samsung Electronics Co., Ltd.
  *
